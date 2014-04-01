436b89bac2307842ed993f650b7e64b43aa8967c
Kyle Granat <kyle@trossenrobotics.com>
2014-04-01 14:07:13 -0500

    Added serial version of dynapose

--- dynaPoseSerial/dynaPoseSerial.ino
+++ dynaPoseSerial/dynaPoseSerial.ino
@@ -7,7 +7,7 @@
 
 
 
-#define SERVOCOUNT 6
+#define SERVOCOUNT 5
 #define LED_PIN 0
 #define TORQUE_BUTTON_PIN  1
 #define ENTER_BUTTON_PIN  2
@@ -18,6 +18,7 @@
 
 #define NUMBER_OF_MODES 4;
 
+#define MAX_POSES 5
 //#define MAX_POSES 5
 
 
@@ -43,11 +44,29 @@ long debounceDelay = 50;    // the debounce time; increase if the output flicker
 
 int reading[5];
   
-int MAX_POSES  = 5;
+//int MAX_POSES  = 5;
+
+//int i = 0; //counter for renaming 
   
 //int poses[MAX_POSES][SERVOCOUNT+1]; //10 poses
+
+
+int poseData[SERVOCOUNT][MAX_POSES] ;
+
+
+
+//int currentPose = 0;
+
 void setup()
 {
+
+  for(int j = 0; j<MAX_POSES;j++)
+  {
+    for(int i = 0; i<SERVOCOUNT;i++)
+    {
+      poseData[i][j] = -1;
+    }
+  }
   
 
 
@@ -55,6 +74,16 @@ void setup()
 
   
     Serial.println("DYNAPose");
+    Serial.println("-----OPTIONS-----");
+    Serial.println("1: Relax Servos");
+    Serial.println("2: Enable Torque and Report Servo Position");
+    Serial.println("3: Save Current position");
+    Serial.println("4: Display Sequence");
+    Serial.println("5: Play Sequence Once");
+    Serial.println("Coming Soon");
+    Serial.println("6: Change Speed");
+    Serial.println("7: Set Next Pose");
+    
   
     Serial.println("Enter '1' to relax servos. Enter '2' to turn on torque and report position.");
   
@@ -73,22 +102,33 @@ void loop()
   switch (inByte)
   {
     case '1':    
-      RelaxServos();
+      relaxServos();
     break;    
 
     case '2':
-      TorqueServos(); 
+      torqueServos(); 
     break;
 
     case '3':
-      PoseToSequence(); 
-    break;2
+      savePose();
+    break;
+    
+    case '4':
+      displayPoses();
+    break;
+    
+    
+    case '5':
+      playPoses();
+    break;
+    
+    
     
   } 
 }
 
 
-void RelaxServos()
+void relaxServos()
 {
   
     Serial.println("Servos Relaxed");
@@ -98,21 +138,21 @@ void RelaxServos()
       int id = i+1;
       Relax(id);
 
-      delay(50);
+      delay(10);
     }
   
   
 }
     
 
-void TorqueServos()
+void torqueServos()
 {
     for(int i = 0; i<SERVOCOUNT;i++)
     {
       int id = i+1;
       TorqueOn(id);
 
-      delay(50);
+      delay(10);
     }
  
  
@@ -129,12 +169,113 @@ void TorqueServos()
     }
   }
     Serial.println("};");
- 
- 
-
- 
+  
   
 }
+    
+    
+    
+void savePose()
+{
+  Serial.print("Saving Pose #");
+  Serial.println(currentPose);
+  
+  for(int i = 0; i<SERVOCOUNT;i++)
+  {
+    int id = i+1;
+    poseData[i][currentPose] = (GetPosition(id));
+  }
+  
+  currentPose = (currentPose + 1)%MAX_POSES;
+  
+
+  Serial.print("The Next save #");
+  Serial.println(currentPose);
+  
+}
+
+
+
+void displayPoses()
+{   
+    
+  
+  for(int j = 0; j<MAX_POSES;j++)
+  {
+    for(int i = 0; i<SERVOCOUNT;i++)
+    {
+      Serial.print(poseData[i][j]);
+      Serial.print(" ");
+    }
+    Serial.println("");
+  }
+  
+}
+    
+    
+void playPoses()
+{   
+  
+  bioloid.poseSize = SERVOCOUNT;//
+
+
+   for(int j = 0; j<MAX_POSES;j++)
+  {  
+    Serial.print("Pose #");
+    Serial.println(j);
+    
+    bioloid.readPose();//find where the servos are currently
+ 
+ 
+    int errorFlag = 0;
+    
+        for(int i = 0; i<SERVOCOUNT;i++)
+        {
+          int id = i+1;
+         // SetPosition(id, positions[i]);
+          Serial.println(poseData[i][j]);
+           bioloid.setNextPose(id,poseData[i][j]);
+           if(poseData[i][j] == -1)
+           {
+              errorFlag = 1; 
+           }
+         }
+
+    if(errorFlag == 0)
+    {
+        bioloid.interpolateSetup(2000); // setup for interpolation from current->next over 1/2 a second
+         while(bioloid.interpolating > 0)
+         {  // do this while we have not reached our new pose
+           bioloid.interpolateStep();     // move servos, if necessary. 
+           delay(3);
+         }
+      
+    }
+    
+    else
+    {
+      Serial.println("Error, cannot complete pose");
+    }
+  
+                
+    
+       
+  }
+  
+  
+  
+  for(int j = 0; j<MAX_POSES;j++)
+  {
+    for(int i = 0; i<SERVOCOUNT;i++)
+    {
+      Serial.print(poseData[i][j]);
+      Serial.print(" ");
+    }
+    Serial.println("");
+  }
+  
+}
+    
     
 
 
