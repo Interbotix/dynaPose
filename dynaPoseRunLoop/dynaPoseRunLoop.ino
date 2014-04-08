ff2926908c02c26bd79f5b46adca5e45ab514403
Kyle Granat <kyle@trossenrobotics.com>
2014-04-08 12:06:44 -0500

    Cleanup

--- dynaPoseRunLoop/dynaPoseRunLoop.ino
+++ dynaPoseRunLoop/dynaPoseRunLoop.ino
@@ -3,58 +3,49 @@
 #include <avr/pgmspace.h>        //PROGMEM library allows the ArboitX to store poses in Flash memory, so that SRAM is not used
 
 #define SERVO_COUNT 6            //the number of servos that will be attached - change this for your specific robot
+#define POSE_COUNT 5
+
+#define DEFAULT_BIOLID_DELAY 1000  //the time in ms it will take to move from one pose to the next
+#define DEFAULT_POSE_DELAY 1000    //the time to hold a pose before moving to the next pose
 
 BioloidController bioloid = BioloidController(1000000);  //initialize the bioloid controller to 1000000 baud
 
-//PROGMEM prog_uint16_t poseTemp1[] = {};  // place-holder for your specific pose. Replace this line with one received from the ArbotiX running DYNAposeSerial 
-
-//EXAMPLES
-//PhantomX RobotTurret Example
-//PROGMEM prog_uint16_t poseTemp1[] = {2,512,512};
-
-//WidowX/ScorpionX RobotTurret Example
-//PROGMEM prog_uint16_t poseTemp1[] = {2,2048,2048};
-
-//PhantomX Pincher Arm Example
-
-//PhantomX Reactor  Arm Example
-
-//WidowX Arm Example
-PROGMEM prog_uint16_t poseTemp1[] = {6,2130,1302,1413,1815,513,510};
-
-
-//PhantomX Quadruped Example
-
-//PhantomX Hexapod Example
-
-
-
 void setup()
 {
-  delay(100);  //delay to wait for DYNAMIXEL initialization    
-  
-  bioloid.poseSize = SERVO_COUNT;  // define the poseSize for the bioloid controller
-  bioloid.loadPose(poseTemp1);     // load the pose from FLASH, into the nextPose buffer
-  bioloid.readPose();              // read in current servo positions to the curPose buffer 
-  bioloid.interpolateSetup(1000); // setup for interpolation from current->next over 1 second
-  
-  while(bioloid.interpolating > 0)
-  {  // do this while we have not reached our new pose
-     bioloid.interpolateStep();     // move servos, if necessary. 
-     delay(3);
-  }
-     
+  delay(100);  //delay to wait for DYNAMIXEL initialization         
 }
 
 
 void loop() 
 { 
-   //do nothing
+  
+   playPoseOnce(pose1, DEFAULT_BIOLID_DELAY );
+   delay(DEFAULT_POSE_DELAY)
+   
 }
 
+    
+int playPoseOnce( const unsigned int * pose, int bioloidDelay)
+{   
+  bioloid.poseSize = SERVO_COUNT;  // define the poseSize for the bioloid controller
+
+  for(int j = 0; j<POSE_COUNT;j++)
+  {  
+
+    bioloid.loadPose(poseTemp1);     // load the pose from FLASH, into the nextPose buffer
+    bioloid.readPose();              // read in current servo positions to the curPose buffer 
+    bioloid.interpolateSetup(1000); // setup for interpolation from current->next over 1 second
+      
+    while(bioloid.interpolating > 0)
+    {  // do this while we have not reached our new pose
+      bioloid.interpolateStep();     // move servos, if necessary. 
+      delay(3);
+    }     
+  }  
+}
+  
 
 
-    
-    
+
 
 
